711d76ea6cffd285b7be05bd809957e6af70d3a4
CooperWoro <cworobetz@gmail.com>
2016-02-10 11:37:45 -0800

    Added beginning of equity calculator to welcome.php

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,52 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+//NOTE that it extends application, not CI_CONTROLLER
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+class Welcome extends Application {
+	function __construct(){
+		parent::__construct();
+	}
+	
+
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		//uncommenting these lines makes it seem like you are logged in,
+		//to see what it looks like 
+		$_SESSION['loggedIn'] = true;
+		$_SESSION['username'] = 'Mickey';
+		
+		
+		
+		$this->data['pagebody'] = 'welcome_message'; //setting view to use
+		$this->data['title'] = 'Bot Assembler'; //Changing nav bar to show page title
+		if($_SESSION['loggedIn']){
+			//setting appropriate data that
+			$table = "";
+			$collection = $this->collections->collection_by_player($_SESSION['username']);
+			
+			foreach($collection as $row){
+				$table .= "<div>Player " . $row['Player'] . " has piece " . $row['Piece'] . " with token " . $row['Token'] . " from the time of " . $row['DateTime'] . "</div>";
+			}
+			
+			$this->data['inventory_table'] = $table;
+			$this->Render();
+			$this->findCardSet();
+		}
 	}
+	
+	public function findCardSet()
+	{
+		$allCards = $this->collections->all();
+		
+		foreach($allCards as $card) {
+			$Piece = preg_match('/([0-9]){2}/', $card['Piece']);
+			$subPiece = preg_match('/[a-z]/', $card['Piece']);
+			$cardPosition = preg_match('/\d{1}$/', $card['Piece']);
+			var_dump($subPiece);
+		}
+		$allCards[0];
+	}
+	
+	
 }
