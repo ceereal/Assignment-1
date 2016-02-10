25d3cb2dc2e28aff35eebd2211c166b0f8af40d7
labsquid <labsquidthrowaway@gmail.com>
2016-02-10 11:58:05 -0800

    Fixed models for new database
    Signed-off-by: labsquid <labsquidthrowaway@gmail.com>

--- application/models/series.php
+++ application/models/series.php
@@ -1,13 +1,8 @@
 <?php
 class Series extends CI_Model {
 
-	/*Example usage in a controller:
+
 	
-	$series = $this->series->all();
-	foreach($series as $row){
-		$table .= "<div>There is a series " . $row['Series'] . " called  " . $row['Description'] . " with frequency " . $row['Frequency'] . " and value " . $row['Value'] . "</div>";
-	}
-	*/
 	//constructor as good practice
 	function __construct(){
 		
@@ -18,13 +13,13 @@ class Series extends CI_Model {
 	// Returns all series
 	// Where clause removes the first header row
 	function all(){
-		$query = $this->db->query("SELECT `COL 1` AS Series, `COL 2` AS Description, `COL 3` AS Frequency, `COL 4` AS Value  FROM series WHERE `COL 1` != 'Series';");
+		$query = $this->db->query("SELECT * from series;");
 		return $query->result_array();
 	}
 	
 	//returns a single series based on it's id number
 	function series_by_id($id){
-		$query = $this->db->query("SELECT `COL 1` AS Series, `COL 2` AS Description, `COL 3` AS Frequency, `COL 4` AS Value FROM series WHERE `Col 1` = '$id';");
+		$query = $this->db->query("SELECT * FROM series WHERE Series = $id");
 		return $query->row_array();
 	}
 }
--- application/models/transactions.php
+++ application/models/transactions.php
@@ -1,13 +1,6 @@
 <?php
 class Transactions extends CI_Model {
 	
-	/*Example usage in a controller:
-	
-	$transactions = $this->transactions->transactions_by_player("Mickey");
-	foreach($transactions as $transaction){
-		$table .= "<div>At " . $transaction['DateTime'] . " " . $transaction['Player'] . " " . $transaction['Transaction'] . " a series " . $transaction['Series'] . "</div>";
-	}
-	*/
 	
 	//constructor as good practice
 	function __construct(){
@@ -19,21 +12,21 @@ class Transactions extends CI_Model {
 	//Returns all transactions in descending order by post date
 	//Where clause removes the title row
 	function all(){
-		$query = $this->db->query("SELECT `COL 1` AS DateTime, `COL 2` AS Player, `COL 3` AS Series, `COL 4` AS Transaction FROM transactions WHERE `COL 1` != 'DateTime' ORDER BY `COL 1` DESC;");
+		$query = $this->db->query("SELECT * FROM transactions ORDER BY DateTime DESC;");
 		
 		return $query->result_array();
 	}
 	
 	//gets all transactions for a single player
 	function transactions_by_player($playerName){
-		$query = $this->db->query("SELECT `COL 1` AS DateTime, `COL 2` AS Player, `COL 3` AS Series, `COL 4` AS Transaction FROM transactions WHERE `COL 2` = '$playerName';");
+		$query = $this->db->query("SELECT * FROM transactions WHERE Player = '$playerName';");
 		return $query->result_array();
 	}
 	
 	// gets all transactions of a type
 	// can be either 'sell' or 'buy'
 	function transactions_by_type($type){
-		$query = $this->db->query("SELECT `COL 1` AS DateTime, `COL 2` AS Player, `COL 3` AS Series, `COL 4` AS Transaction FROM transactions WHERE `COL 4` = '$type';");
+		$query = $this->db->query("SELECT * FROM transactions WHERE Trans = '$type';");
 		return $query->result_array();
 	}
 }
