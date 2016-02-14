9545bad48befe18e17a643ae4ec40532eb8d74e1
CooperWoro <cworobetz@gmail.com>
2016-02-14 11:25:05 -0800

    Fixed up php to match new database, removed old stuff that did not end up working

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -26,53 +26,13 @@ class Welcome extends Application {
 			$collection = $this->collections->collection_by_player($_SESSION['username']);
 			
 			foreach($collection as $row){
-				$table .= "<div>Player " . $row['Player'] . " has piece " . $row['Piece'] . " with token " . $row['Token'] . " from the time of " . $row['Datetime'] . "</div>";
+				$table .= "<div>Player " . $row['Player'] . " has piece " . $row['Series'] . $row['SubSeries'] . "-" . $row['CardPosition'] . " with token " . $row['Token'] . " from the time of " . $row['Datetime'] . "</div>";
 			}
 			
 			$this->data['inventory_table'] = $table;
 			$this->Render();
-			$this->findCardSet();
+			//$this->findCardSet();
 		}
-	}
-	
-	//Find equity of all players
-	//If they have a collection -> add value of collection to equity
-	//If they don't have a collection -> add +1 peanut for each card
-	//To calc peanuts, call this, find how many of A, B, and C you have,
-	//(IE 3 top, 2 bottom, 4 middle = 2 complete sets), multiply by 50, 
-	//subtract #sets * 3 from total, add that to #	peanuts. Done!
-	public function findCardSet(){
-		$allCards = $this->collections->all();
-		/*
-		foreach($allCards as $card){
-			$players = $this->collections->get_players();
-			foreach($players as $player){
-				$sets = $this->collections->get_set_by_player_piece_subpiece($player['Player'], $card['Piece'], $card['SubPiece']);
-				foreach($sets as $set){
-					echo '<pre> ' . $player['Player'] . ' ' . var_export($set, true) . '</pre>';
-				}
-				
-			}
-		}*/
-		
-		$players = $this->collections->get_players();
-		
-		foreach($players as $player){
-			//echo $player['Player'];
-			$playerCards = $this->collections->collection_by_player($player['Player']);
-			$distinctSets = $this->collections->get_distinct_sets();
-			foreach($distinctSets as $set){
-				$playerSets = $this->collections->get_set_by_player_piece_subpiece($player['Player'], $set['Piece'], $set['SubPiece']);
-				echo '<pre> ' . var_export($playerSets, true) . '</pre>';
-			}
-			
-			
-			
-			
-			
-			break;
-		}
-	}
-	
+	}	
 	
 }
