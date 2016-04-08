bfd544c584bb384c5a2fda5cfcd054a553f056f0
labsquid <labsquidthrowaway@gmail.com>
2016-04-07 20:21:05 -0700

    Changed collections to be based on CSV, made a parent model that represents CSV data eaiser, fixed Welcome.php field references
    Signed-off-by: labsquid <labsquidthrowaway@gmail.com>

--- application/models/series.php
+++ application/models/series.php
@@ -1,42 +1,16 @@
 <?php
-class Series extends CI_Model {
-
-
+class Series extends MY_Model {
 
 	//constructor as good practice
 	function __construct(){
-
-		parent::__construct();
+		parent::__construct("http://botcards.jlparry.com/data/series");
 
 	}
 
 	// Returns all series
 	// Where clause removes the first header row
 	function all(){
-		$return;
-		$count = 0;
-
-		$definition;
-		//Check if the handle can open
-		if (($handle = fopen("http://botcards.jlparry.com/data/series", "r")) !== FALSE) {
-			//If the handle opened, go through each line as a csv file
-		    while (($data = fgetcsv($handle, ",")) !== FALSE) {
-				//the first row defines the table
-				if($count == 0){
-					$definition = $data;
-				}
-				//all other rows use the definition to assign values
-				else{
-					$tempRow;
-					foreach($definition as $key=>$field){
-						$tempRow[$field] = $data[$key];
-					}
-					$return[$count] = $tempRow;
-				}
-				$count++;
-			}
-		}
-		fclose($handle);
+		$return = $this->get_all();
 
 		return $return;
 	}
@@ -44,34 +18,7 @@ class Series extends CI_Model {
 	//returns a single series based on it's id number/code
 	function series_by_id($id){
 
-		$return;
-		$count = 0;
-
-		$definition;
-		//Check if the handle can open
-		if (($handle = fopen("http://botcards.jlparry.com/data/series", "r")) !== FALSE) {
-			//If the handle opened, go through each line as a csv file
-		    while (($data = fgetcsv($handle, ",")) !== FALSE) {
-				//the first row defines the table
-				if($count == 0){
-					$definition = $data;
-				}
-
-				else{
-					//prepares a temp row using the definition
-					$tempRow;
-					foreach($definition as $key=>$field){
-						$tempRow[$field] = $data[$key];
-					}
-					//applies logic to the temp row to see if it should select it
-					if($temp["code"] == $id){
-						$return[$count] = $tempRow;
-					}
-				}
-				$count++;
-			}
-		}
-		fclose($handle);
+		$return = $this->get_limited("code", $id);
 
 		return $return;
 	}
