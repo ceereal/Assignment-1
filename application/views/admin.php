4d4c27bb8e54a0122be05efbe0f48d2fca71ac55
cooperworo <cworobetz@gmail.com>
2016-04-08 13:48:02 -0700

    Made admin function to register database, added listing users

--- application/views/admin.php
+++ application/views/admin.php
@@ -1,5 +1,8 @@
 <div id="app-registration">
     <form action="/Admin/register" method="POST">
+        <div><input type="text" name="team" value="B17"/></div>
+        <div>Team Name: <input type="text" name="name"/></div>
+        <div>Password: <input type="password" name="password"/></div>
         {registerButton}
     </form>
 </div>
