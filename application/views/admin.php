9e6667ecc5ebc1ff914147c8b3d68c9943f43b6e
labsquid <labsquidthrowaway@gmail.com>
2016-04-07 21:07:21 -0700

    Added barebones controllers (just html + some css) for registration and administration
    Signed-off-by: labsquid <labsquidthrowaway@gmail.com>

---
+++ application/views/admin.php
@@ -0,0 +1,10 @@
+<div id="player-table">
+    <table>
+        <tr>
+            <th>Player</th>
+            <th></th>
+            <th></th>
+        </tr>
+        {tableData}
+    </table>
+</div>
